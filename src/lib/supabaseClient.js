670d92707270bc1245e8e6d6f48bf285b4cdbc55
kobe opdekamp <kobe.opdekamp@student.uhasselt.be>
2024-06-01 19:51:09 +0200

    commit

--- src/lib/supabaseClient.js
+++ src/lib/supabaseClient.js
@@ -1,4 +1,4 @@
-import { PUBLIC_SUPABASE_SERVER, PUBLIC_SUPABASE_ANON_KEY } from '$env/static/public';
+import { PUBLIC_SUPABASE_URL, PUBLIC_SUPABASE_ANON_KEY } from '$env/static/public';
 import { createClient } from '@supabase/supabase-js';
 
-export const db = createClient(PUBLIC_SUPABASE_SERVER, PUBLIC_SUPABASE_ANON_KEY);
+export const db = createClient(PUBLIC_SUPABASE_URL, PUBLIC_SUPABASE_ANON_KEY);
